ec104c8d7805387005da1ddc557f0ae8e7cf5e3d
frnzdev <felipeacfornazeiro@gmail.com>
2025-04-06 15:17:27 -0300

    definindo tipagem de produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import { ProductType } from "@/types/ProductType";
 import Image from "next/image";
 
-export default function Home() {
+async function getProducts() {
+  const res = await fetch("https://fakestoreapi.com/products");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const product = await getProducts();
+  console.log(product);
   return (
-    <div>
-      <h1 className="text-red-500">TESTE</h1>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-10 xl:gap-6">
+        {product.map((product: ProductType) => (
+          <div key={product.id}>{product.title}</div>
+        ))}
+      </div>
     </div>
   );
 }
